8efdbd02e49e31320d4d1fcc021f65ecf4a92002
Linh Nguyen <linh0404@hotmail.com>
2019-07-18 13:49:13 +1000

    added pseucode for javascript section

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// PSEUDOCODE
+// clicking the "start quiz" button will display the main page i.e heading, score, questions, answers and charge
+// list questions in an object/array containing question, choices and correct answer
+// record userchoice and tally if correct
+// have a timer for each question ~ 30 seconds
+// user must choose an option before time runs out
+// if time runs out and user hasnt chosen, it will move to the next question and score is 0
+// last question is chosen - results will appear as percentage
