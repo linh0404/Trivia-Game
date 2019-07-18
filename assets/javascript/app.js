1de3d73e129c634c5b72ad24ab5d77672c2e410e
Linh Nguyen <linh0404@hotmail.com>
2019-07-18 14:49:46 +1000

    created variables for the JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,50 @@
 // user must choose an option before time runs out
 // if time runs out and user hasnt chosen, it will move to the next question and score is 0
 // last question is chosen - results will appear as percentage
+
+"start"
+"quiz"
+"question"
+"counter"
+"timer"
+"A"
+"B"
+"C"
+"D"
+"progress"
+
+
+let questions = [
+    {
+        question: "What is name of the time travelling vehicle?" 
+        choiceA: "Time-Turner",
+        choiceB: "Tardis",
+        choiceC: "Delorean",
+        choiceD: "Time Displacement Equipment"
+        correct: "C"
+    },
+    {
+        question: "Question" 
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correct: "A"
+    },
+    {
+        question: "Question" 
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correct: "D"
+    },
+    {
+        question: "Question" 
+        choiceA: "A",
+        choiceB: "B",
+        choiceC: "C",
+        choiceD: "D",
+        correct: "B"
+    }
+]
